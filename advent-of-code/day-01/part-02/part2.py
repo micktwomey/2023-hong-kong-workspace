69783e081b28d28d75ae76fc5c952586abe3958b
Michael Twomey <mick@twomeylee.name>
2023-12-08 22:59:18 +0800

    Fix handling of overlapping matches
    Puzzle treats oneight as one, eight, not one. Change regex to find all overlapping matches.

--- advent-of-code/day-01/part-02/part2.py
+++ advent-of-code/day-01/part-02/part2.py
@@ -20,23 +20,33 @@ LOOKUP = {
 }
 
 
+def get_matches(s: str) -> list[str]:
+    return [
+        x
+        for x in re.findall(
+            r"(?=([0-9]|one|two|three|four|five|six|seven|eight|nine|zero))",  # find overlapping matches, see https://stackoverflow.com/questions/11430863/how-to-find-overlapping-matches-with-a-regexp
+            # r"([0-9]|one|two|three|four|five|six|seven|eight|nine|zero)",
+            s,
+        )
+        if x
+    ]
+
+
 def main():
     numbers: list[int] = []
     for line in sys.stdin:
-        matches = [
-            x
-            for x in re.findall(
-                r"([0-9]|one|two|three|four|five|six|seven|eight|nine|zero)",
-                line,
-            )
-            if x
-        ]
+        matches = get_matches(line)
         first = LOOKUP.get(matches[0], matches[0])
         last = LOOKUP.get(matches[-1], matches[-1])
         number = int(f"{first}{last}")
         numbers.append(number)
-        LOG.info((line, matches, first, last, number))
+        LOG.info(
+            (line, matches, (first, last), number),
+            single=len(matches) == 1,
+            equal=(first == last),
+        )
     LOG.info("total", total=sum(numbers))
+    assert get_matches("oneight") == ["one", "eight"], get_matches("oneight")
 
 
 if __name__ == "__main__":
